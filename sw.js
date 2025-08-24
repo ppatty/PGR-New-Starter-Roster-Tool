6792a237b4264c3b4f24a09b53479d4a317aa478
ppatty <ppatty@live.com.au>
2025-08-24 13:41:11 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,49 @@
+// A simple cache-first service worker
+const CACHE_NAME = 'pgr-roster-cache-v2';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'manifest.webmanifest'
+  // Icons are not cached here but can be added if you create them
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
